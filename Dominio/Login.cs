eb687e81239a43ac3c455cf058e0c3e0546d66cf
yujifelipe <yuji.kuroda@gmail.com>
2018-11-11 21:23:30 -0400

    Contexto

---
+++ Dominio/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dominio
+{
+    public class Login
+    {
+        public Login(int? id, string email, string senha)
+        {
+            Id = id;
+            Email = email;
+            Senha = senha;
+        }
+
+        public Nullable<int> Id { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Senha { get; private set; }
+    }
+}
---
+++ Infra/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Dominio;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace Infra
+{
+    public class Contexto : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public Contexto(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<Login> Login { get; set; }
+
+        private void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conexao = new SqlConnectionStringBuilder()
+            {
+                DataSource = "YUJIPC",
+                InitialCatalog = "SistemaDeAutenticacao",
+                IntegratedSecurity = true
+            };
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Contexto"));
+        }
+    }
+}
